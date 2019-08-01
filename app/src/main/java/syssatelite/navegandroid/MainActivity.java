2a834623db6ad8738f89d6a7334a41cdbfd022e2
Paloma <paloma>
2019-08-01 09:51:47 -0300

    telas sobre e menu

--- app/src/main/java/syssatelite/navegandroid/MainActivity.java
+++ app/src/main/java/syssatelite/navegandroid/MainActivity.java
@@ -1,7 +1,11 @@
 package syssatelite.navegandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.configurar:
+                //newGame();
+                return true;
+            case R.id.localizar:
+                //showHelp();
+                return true;
+            case R.id.sobre:
+                Intent intent = new Intent(MainActivity.this, telaSobre.class);
+                startActivity(intent);
+                return true;
+            default:
+                return true;
+
+        }
     }
 }
