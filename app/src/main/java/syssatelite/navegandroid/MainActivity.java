215b11980d3aa7410ba8cc518053d00e587026cc
Paloma <palomacorreiadasilva@gmail.com>
2019-08-13 23:39:31 -0300

    estrutura de dados para satelite

--- app/src/main/java/syssatelite/navegandroid/MainActivity.java
+++ app/src/main/java/syssatelite/navegandroid/MainActivity.java
@@ -40,10 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent gnss = new Intent(MainActivity.this, TelaGNSS.class);
                 startActivity(gnss);
                 return true;
-            case R.id.ceu:
-                Intent ceu = new Intent(MainActivity.this, SatelitedeCima.class);
-                startActivity(ceu);
-                return true;
+
             case R.id.sobre:
                 Intent sobre = new Intent(MainActivity.this, telaSobre.class);
                 startActivity(sobre);
