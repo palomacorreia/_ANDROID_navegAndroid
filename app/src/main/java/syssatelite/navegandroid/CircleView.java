b47f40df75da946da4650bc6aaea72a376982ffe
Paloma <palomacorreia>
2019-08-15 01:03:40 -0300

    1 bolinha maldita

--- app/src/main/java/syssatelite/navegandroid/CircleView.java
+++ app/src/main/java/syssatelite/navegandroid/CircleView.java
@@ -20,6 +20,9 @@ public class CircleView extends View {
     private String text;      // texto
     private Rect rectangle;
     private Paint paint;
+    public Satelite Sat;
+    public float Xcc, Ycc;
+
 
     // construtor
     public CircleView(Context context, @Nullable AttributeSet attrs) {
@@ -37,53 +40,65 @@ public class CircleView extends View {
 
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        Paint paint=new Paint();
 
-
-        super.onDraw(canvas);
-
-        int x = getWidth();
-        int y = getHeight();
-        int radius;
-        radius = 100;
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.WHITE);
-        canvas.drawPaint(paint);
-        // Use Color.parseColor to define HTML colors
-        paint.setColor(Color.parseColor("#CD5C5C"));
-        canvas.drawCircle(x / 2, y / 2, radius, paint);
-        canvas.drawCircle(x / 2, y / 2, radius*2, paint);
-        canvas.drawCircle(x / 2, y / 2, radius/6, paint);
-
-
-    }
 
     public void SatInfo(Satelite arraySat)
     {
-        double Xc, Yc, Xs, Ys;
-        int radius = 8;
-        int W = getWidth();
-        int H = getHeight();
-        Paint paint=new Paint();
+        //System.out.println("Tamnaho!"+ tamanho);
+        float Xc, Yc, Xs, Ys;
+        int radius = 5, i =0;
+        int W = this.getMeasuredHeight();
+        int H = this.getMeasuredHeight();
+        Paint paint = new Paint();
         Canvas canvas =  new Canvas();
 
-        Xs = radius * Math.cos(arraySat.getELEV()* Math.sin(arraySat.getAZIM()));
-        Ys = radius * Math.cos(arraySat.getELEV()) * Math.cos(arraySat.getAZIM());
+        Xs = (float) (radius * Math.cos((arraySat.getELEV()) * Math.sin(arraySat.getAZIM())));
+        Ys = (float) (radius * Math.cos((arraySat.getELEV()) * Math.cos(arraySat.getAZIM()) ));
 
-        Xc = Xs + W/2;
-        Yc = -Ys + H/2;
+        Xc = Xs + W / 2;
+        Yc = -Ys + H / 2;
 
-        paint.setStyle(Paint.Style.STROKE);
+        paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.BLUE);
 
         canvas.drawPaint(paint);
         // Use Color.parseColor to define HTML colors
         paint.setColor(Color.parseColor("#5ccd5c"));
 
-        canvas.drawCircle((float) Xc, (float)Yc, radius, paint);
+        //canvas.drawCircle( Xc, (float)Yc, radius, paint);
         System.out.println("DESENHEI O SATELITE MEU AMOR!");
+        Xcc = Xc;
+        Ycc = Yc;
+        System.out.println("Xcc" + Xcc);
+        System.out.println("Ycc" + Ycc);
     }
+
+
+    protected void onDraw(Canvas canvas) {
+        Paint paint=new Paint();
+
+
+        super.onDraw(canvas);
+
+        int x = getMeasuredWidth();
+        int y = getMeasuredHeight();
+        int radius;
+        radius = 50;
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.WHITE);
+        canvas.drawPaint(paint);
+        // Use Color
+        // .parseColor to define HTML colors
+        paint.setColor(Color.parseColor("#000000"));
+        canvas.drawCircle(x / 2, y / 2, radius, paint);
+        canvas.drawCircle(x / 2, y / 2, radius*2, paint);
+        canvas.drawCircle(x / 2, y / 2, radius*4, paint);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+        paint.setColor(Color.parseColor("#5ccd5c"));
+
+        canvas.drawCircle(Xcc, Ycc, 20, paint);}
+
+
 
 }
