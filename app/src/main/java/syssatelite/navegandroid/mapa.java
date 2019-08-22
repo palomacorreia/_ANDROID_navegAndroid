eb0763ed4f515c51c6ec624ab115fb8f6163bb86
Paloma <paloma>
2019-08-22 10:59:18 -0300

    Orientações do Mapa

--- app/src/main/java/syssatelite/navegandroid/mapa.java
+++ app/src/main/java/syssatelite/navegandroid/mapa.java
@@ -1,35 +1,38 @@
 package syssatelite.navegandroid;
 
 import android.Manifest;
+import android.app.Service;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Build;
 import android.os.Bundle;
-
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
-public class mapa extends FragmentActivity implements OnMapReadyCallback {
+public class mapa extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
-    /**
-     * Responsável por disponibilizar a localização do smartphone.smartphone
-     */
-    private GoogleApiClient mGoogleApiClient;
+    private LocationManager locationManager;// O Gerente de localização
+    private LocationProvider locProvider; // Provedor de localização
+    private final int REQUEST_LOCATION = 2;
+    private Location userLocation;
+    private LatLng user;
 
-    /**
-     * Guarda a ultima posição do smartphone.
-     */
-    private Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,31 +42,121 @@ public class mapa extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-//
-//        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        mMap.setMyLocationEnabled(true);
-        mMap.setTrafficEnabled(true);
+
+//        // recupera (ou cria) uma instância do arquivo de preferencia do Android,
+//        // pelo seu nome/chave
+//        SharedPreferences pref = getSharedPreferences("configuracoes", MODE_PRIVATE);
+//        // recupera a informação
+//        String grau = pref.getString("grau", null);
+//        String unidade = pref.getString("unidade", null);
+//        String orientacao = pref.getString("orientacao", null);
+//        String tipo = pref.getString("tipo", null);
+//        String ligado = pref.getString("ligado", null);
+//
+//
+//
+//
+//        if(ligado!= null) {
+//            if (ligado.equals("Sim")) {
+//                mMap.setTrafficEnabled(true);
+//            } else {
+//                mMap.setTrafficEnabled(false);
+//            }
+//        }
+
+        //NORTH
+        if(userLocation!=null) {
+
+            user = new LatLng(userLocation.getLatitude(),userLocation.getLongitude());
+        }
+        else {
+
+            user=new LatLng(-12.9531,-38.4589);
+        }
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(user)  //CENTRO DO MAPA
+                .zoom(17)
+                .bearing(90)     //ORIENTAÇÃO DA CÂMERA
+                .build();       //UTILIZA O BUILD PARA CRIAR A CâMERA
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        mMap.addMarker(new MarkerOptions().position(user).title("Sua localização!"));
+
     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        userLocation = location;
+        if (mMap!=null) {
+            mMap.clear();
+            //mMap.addPolyline()
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Permissão do Usuário
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            // A permissão foi dada
+            ativaGPS();
+        } else {
+            // Solicite a permissão
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        desativaGPS();
+    }
+
+    public void ativaGPS() {
+        try {
+            locProvider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+            locationManager.requestLocationUpdates(locProvider.getName(), 30000, 1, this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void desativaGPS() {
+        try {
+            locationManager.removeUpdates(this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
